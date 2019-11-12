304688f1876706ba2692109b72f2a9a6ad188ea1
Seo <icyveins@hotmail.com>
2019-11-12 21:27:28 +0800

    add combinatorial methods for signal/mud

--- cMUDclasses.py
+++ cMUDclasses.py
@@ -34,6 +34,14 @@ class PSKSignal:
         else:
             return self.csi
 
+    def getNumSyms(self):
+        return len(self.syms)
+
+    def getSignalCombinatorics(self):
+        c = np.zeros(self.getNumSyms(), dtype=np.int32)
+        c = c + self.m
+        return c
+
 class OptimalMUD:
     def __init__(self, signals):
         self.signals = signals
@@ -41,5 +49,25 @@ class OptimalMUD:
     def __repr__(self):
         return 'Total signals = ' + str(len(self.signals))
 
+    def getSignalsCombinatorics(self):
+        return np.hstack([i.getSignalCombinatorics() for i in self.signals])
+
+    def addOneToBitSeq(self, bitSeq, combinatorics):
+        bitSeq[0] = bitSeq[0] + 1
+        for i in range(len(bitSeq) - 1):
+            if bitSeq[i] / combinatorics[i] >= 1:
+                bitSeq[i] = 0
+                bitSeq[i+1] = bitSeq[i+1] + 1
+
+
     def ExhaustiveSearch(self, snr=2, iter=1):
+        totalsigs = len(self.signals)
+
+        sigcombis = np.prod(self.getSignalsCombinatorics())
+
+        currBitSequence = self.getSignalsCombinatorics() * 0
+        for i in range(sigcombis):
+            if i != 0: # do not add on the first one
+
+
         noise = 1
--- runClasses.py
+++ runClasses.py
@@ -3,11 +3,27 @@ import scipy as sp
 import cMUDclasses as cm
 import matplotlib.pyplot as plt
 
-qpsk_bits = np.random.randint(0,4,100)
-qpsk_sig = cm.PSKSignal(qpsk_bits, 4)
+numBits = 1
+m = 4 # m-ary signal
+qpsk_bits = np.random.randint(0,m,numBits)
+qpsk_sig = cm.PSKSignal(qpsk_bits, m)
+
+# test some methods
 print(qpsk_sig)
 print(qpsk_sig.getBits())
 print(qpsk_sig.getM())
+print(qpsk_sig.getSignalCombinatorics())
+
+qpsk_bits2 = np.random.randint(0,m,numBits)
+qpsk_sig2 = cm.PSKSignal(qpsk_bits2, m)
+
+cmud = cm.OptimalMUD([qpsk_sig, qpsk_sig2])
+print(cmud.getSignalsCombinatorics())
+
+# test more methods
+testSeq = np.array([3,1], dtype=np.int32)
+cmud.addOneToBitSeq(testSeq, cmud.getSignalsCombinatorics())
+print(testSeq)
 
 fig = plt.figure()
 ax = plt.plot(np.real(qpsk_sig.getSymbols()), np.imag(qpsk_sig.getSymbols()), 'r.')
