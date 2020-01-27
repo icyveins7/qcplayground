b7d678e06ac12260504d37ef973bfd1fb8b3c3d6
icyveins7 <icyveins@hotmail.com>
2020-01-27 18:48:51 +0800

    complete DHAOracle gates
    not fully tested, works on 101, added the rest of the conditions as far as currently tested..
    also added some barriers and QoL text flipping for the diagram

--- qMUDclasses.py
+++ qMUDclasses.py
@@ -3,12 +3,16 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import scipy.signal as sps
 from projectq import MainEngine  # import the main compiler engine
-from projectq.ops import C, X, H, Measure, BarrierGate  # import the operations we want to perform (Hadamard and measurement)
+from projectq.ops import C, X, H, Measure, Barrier  # import the operations we want to perform (Hadamard and measurement)
 from projectq.backends import CircuitDrawer
 
 class DHAOracle:
     def __init__(self, delta, numBits, ancillaBit=None):
         self.C = C
+        self.X = X
+        self.H = H
+        self.Barrier = Barrier
+        
         self.delta = delta
         self.numBits = numBits
         self.delta_bin = np.binary_repr(delta, numBits)
@@ -42,61 +46,112 @@ class DHAOracle:
         prefix = [] # initialize with no prefixes
         
         while (idx<len(delta_bin)):
+            # debug
+            print (idx)
+            
             # Condition 0. For each 0 at the start, place a simple CNOT.
             if delta_bin[idx] == 0:
-                self.gatesList.append(tuple([self.valreg[idx]]) + tuple([self.refreg[cidx]]))
+                print('Condition 0')
+                self.gatesList.append(tuple([self.valreg[prefix[i]] for i in range(len(prefix))]) + tuple([self.valreg[idx]]) + tuple([self.refreg[cidx]]))
                 idx = idx+1 # move to next bit/next iteration
                 cidx = cidx+1
                 
             # Condition 1. All remaining bits are 1. E.g. 11111, 01111 etc.
             elif np.all(delta_bin[idx:]):
+                print('Condition 1')
                 self.gatesList.append(tuple([self.valreg[prefix[i]] for i in range(len(prefix))]) + tuple([self.valreg[i] for i in range(idx, len(delta_bin))]) + tuple([self.refreg[cidx]]))
                 
                 print('Exited at idx ' + str(idx)) # DEBUG
                 
                 idx = len(delta_bin) # end the loop, early stopping
             
-            # Condition 2. Current bit is 1, rest are 0. E.g. 01000 etc.
+            # Condition 2a. Current bit is 1, rest are 0. E.g. 01000 etc.
             elif np.all(np.logical_not(delta_bin[idx+1:])) and delta_bin[idx] == 1:
+                print('Condition 2a')
                 self.gatesList.append(tuple([self.valreg[prefix[i]] for i in range(len(prefix))]) + tuple([self.valreg[idx]]) + tuple([self.refreg[cidx]]))
                 
                 print('Exited at idx ' + str(idx)) # DEBUG
                 
                 idx = len(delta_bin) # early stopping
             
+            # Condition 2b. Current bit is 1. End of sequence.
+            elif (idx == len(delta_bin)-1) and delta_bin[idx] == 1:
+                print('Condition 2b')
+                self.gatesList.append(tuple([self.valreg[prefix[i]] for i in range(len(prefix))]) + tuple([self.valreg[idx]]) + tuple([self.refreg[cidx]]))
+                
+                print('Exited at idx ' + str(idx)) # DEBUG
+                
+                idx = len(delta_bin) # early stopping
+                
+            # Condition 3. Current bit is 1. All special cases failed.
+            elif (delta_bin[idx] == 1):
+                print('Condition 3')
+                prefix.append(idx)
+                idx = idx + 1
+                # but we don't need to move cidx
+                
+            # Condition 4. Current bit is 0. All special cases failed.
+            elif (delta_bin[idx] == 0):
+                print('Condition 4')
+                # attach all existing prefixes, and write a toffoli with a node at the 0
+                self.gatesList.append(tuple([self.valreg[prefix[i]] for i in range(len(prefix))]) + tuple([self.valreg[idx]]) + tuple([self.refreg[cidx]]))
+                idx = idx + 1
+                
             else:
-                print('not yet implemented')
-                idx = len(delta_bin)
-            
-#            # Condition 2. All remaining bits are 0. E.g. 10000, 01000 etc.
-#            elif np.all(np.logical_not(delta_bin[idx:])):
-#                gateList.append(tuple([self.valreg[prefix[i]] for i in range(len(prefix))]) + tuple([self.valreg[i] for i in range(len())]))
+                print('Unknown conditions')
 
+        # implement the gates
         for i in range(len(self.gatesList)):
             print(self.gatesList[i])
             
             # implement the gate
             self.C(X,len(self.gatesList[i])-1) | self.gatesList[i]
-            # self.C(X, len(self.gatesList[i]) - 1) | self.gatesList[i]
-
-        # debug, cant seem to add another gate?
-        self.C(X, 1) | (self.refreg[1], self.ancilla[0])
-
-        # # implement the centre toffoli
-        # print('Not yet implemented centre toffoli, just for testing..')
-        # self.C(X, sum(self.delta_bin)) | (self.refreg[1], self.ancilla)
-        #
-        #
-        #
-        # implement the reverse of the toffoli gates to complete the oracle
-        # for i in range(len(self.gatesList)-1, -1, -1):
-        # for i in range(len(self.gatesList)):
-        #     print(self.gatesList[i])
-        #
-        #     self.C(X,len(self.gatesList[i])-1) | self.gatesList[i]
+            
+        # implement X gates on 0 bits
+        for i in range(len(delta_bin)):
+            if (delta_bin[i] == 0):
+                self.X | self.refreg[i]
+                
+        # Barrier for neatness?
+        self.Barrier | (self.valreg, self.refreg, self.ancilla)
+                
+        # implement Hadamard on ancilla
+        self.H | self.ancilla
+            
+        # implement centre Toffoli
+        self.C(X,len(delta_bin)) | (self.refreg, self.ancilla)
+        
+        # == and then implement all the reverses ==
+        # implement Hadamard on ancilla
+        self.H | self.ancilla
+        
+        # Barrier for neatness?
+        self.Barrier | (self.valreg, self.refreg, self.ancilla)
+        
+        # implement X gates on 0 bits
+        for i in range(len(delta_bin)):
+            if (delta_bin[i] == 0):
+                self.X | self.refreg[i]
+                
+        # implement the gates
+        for i in range(len(self.gatesList)-1, -1, -1):
+            print(self.gatesList[i])
+            
+            # implement the gate
+            self.C(X,len(self.gatesList[i])-1) | self.gatesList[i]
+        
+        
             
         # flush gates
         self.diag_eng.flush()
         
-    def showGates(self):
-        print(self.circuit_drawer.get_latex())
+    def showGates(self, adjustBarrier=False):
+        text2print = self.circuit_drawer.get_latex()
+        
+        if adjustBarrier:
+            # fix the barrier text to be vertical
+            text2print = text2print.replace("Barrier", "\\rotatebox{90}{Barrier}")
+            
+        print(text2print)
+        
+        return text2print
--- run_qMUDClasses.py
+++ run_qMUDClasses.py
@@ -4,12 +4,13 @@ import scipy.signal as sps
 import qMUDclasses as qm
 
 #delta_list = np.arange(1,32)
-delta_list = np.array([2])
+#delta_list = np.array([3])
+delta_list = np.array([5])
 
 for i in range(len(delta_list)):
     delta = delta_list[i]
-    oracle = qm.DHAOracle(delta, 5)
+    oracle = qm.DHAOracle(delta, 3)
     print(oracle.delta_bin)
     oracle.makeGates()
-    oracle.showGates()
+    txt = oracle.showGates(adjustBarrier=True)
     
